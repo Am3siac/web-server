032482a840d711087f010033aadeb007e71048e7
Luca Galati <memento.reloaded@gmail.com>
2016-05-18 17:29:45 +0200

    Update language 2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var middleware = require('./middleware.js');
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About us!');
+	res.send('About Us!');
 
 })
 
